0edc0190a91ec66312f530d2e6df9419dc5e57ad
Soulmaz <somisomi76@yahoo.com>
2022-05-31 11:24:03 +0200

    https://github.com/somi2022/https---github.com-somi2022-NewQuiz

---
+++ main.js
@@ -0,0 +1,24 @@
+function check(){
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var correct = 0;
+    
+    if ( question1 == "Biden") {
+        correct++;
+    }
+    if ( question2 == "Blair") {
+        correct++;
+    }
+    if ( question3 == "Stefan Loven") {
+        correct++;
+    }
+    if ( question4 == "Tesla") {
+        correct++;
+    }
+    if ( question5 == "Sweden") {
+        correct++;
+    }
+    
